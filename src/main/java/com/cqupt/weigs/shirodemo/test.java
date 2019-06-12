f1ca74236b5d61b87c421f7e45130c9e9d29e924
weiguangshuai <weiguangshuai@richstonedt.com>
2019-06-12 12:30:19 +0800

    Revert "Revert "add test02""
    This reverts commit 5f236f7e

--- src/main/java/com/cqupt/weigs/shirodemo/test.java
+++ src/main/java/com/cqupt/weigs/shirodemo/test.java
@@ -7,5 +7,6 @@ package com.cqupt.weigs.shirodemo;
 public class test {
     public static void main(String[] args) {
         System.out.println("test01");
+        System.out.println("test02");
     }
 }
